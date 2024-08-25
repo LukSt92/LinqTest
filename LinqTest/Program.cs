be6513849594fddf2c8e66701e1bff7db8fcdfc8
LukSt92 <lukasz.m.stasiak@gmail.com>
2024-08-25 12:03:27 +0200

    Add: basic Linq commands.

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -10,6 +10,23 @@ namespace LinqTest
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 99; i++)
+                numbers.Add(i);
+            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+            foreach (int i in firstAndLastFive)
+                Console.Write($"{i} ");
+            Console.WriteLine("");
+
+            int[] values = new int[] {0, 12, 44,36,92,54,13,8 };
+            IEnumerable<int> result =
+                from v in values
+                where v < 37
+                orderby -v
+                select v;
+
+            foreach (int i in result)
+                Console.Write($"{i} ");
         }
     }
 }
